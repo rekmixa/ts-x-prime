c3fb7928c8198daa047c289d1a24ab45180fd6d0
rekmixa <rekmixa@gmail.com>
2025-07-30 20:42:10 +0500

    build sources to dist

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,3 +1,46 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.ts
+var index_exports = {};
+__export(index_exports, {
+  Complex: () => Complex,
+  addPolynomials: () => addPolynomials,
+  checkPolynomialCongruence: () => checkPolynomialCongruence,
+  eta: () => eta,
+  findCandidateR: () => findCandidateR,
+  gamma: () => gamma,
+  gcd: () => gcd,
+  generateSieve: () => generateSieve,
+  isPerfectPower: () => isPerfectPower,
+  isPrime: () => isPrime,
+  isPrimeAKS: () => isPrimeAKS,
+  isPrimeSieve: () => isPrimeSieve,
+  modularPolynomialExponentiation: () => modularPolynomialExponentiation,
+  multiplyPolynomials: () => multiplyPolynomials,
+  sieveToPrimes: () => sieveToPrimes,
+  zeta: () => zeta,
+  zetaEuler: () => zetaEuler,
+  zetaReal: () => zetaReal
+});
+module.exports = __toCommonJS(index_exports);
+
 // src/basics.ts
 function gcd(a, b) {
   while (b !== 0) {
@@ -341,7 +384,8 @@ function zeta(s, terms = 1e4) {
     return front.mul(zetaMirror);
   }
 }
-export {
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
   Complex,
   addPolynomials,
   checkPolynomialCongruence,
@@ -360,4 +404,4 @@ export {
   zeta,
   zetaEuler,
   zetaReal
-};
+});
